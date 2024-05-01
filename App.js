f1c053e8179d4dc434e1f61413571414f7157608
Balajee Reddy Karra <balajeereddy.iiit@gmail.com>
2024-05-01 22:59:58 +0530

    Added more code

--- App.js
+++ App.js
@@ -9,8 +9,38 @@ import ReactDOM from "react-dom/client";
 // JSX (It's transpiled the code before reaches JS Engine) -- PARCEL - Babel
 // JSX ==> React.createElement ==> Object ==> HTML element(render)
 
-const jsxheading=<h1 className="heading" tabIndex="1">Namasthe React using JSX</h1>;
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// React Component
+// Class Based Component - Old
+// Functional Based Component - New
+
+// React Functional Component
+
+// Component Composition --> using 2 components in a single place.
+
+const elem=<span>React Element </span> 
+
+const Title = () => (<h1 className="heading" tabIndex="1">
+    {elem}
+    Namasthe React using JSX
+    </h1>
+);
+
+const title = (<h1 className="heading">Hello Balajee K</h1>);
+
+const num=1000;
+const HeadingComponent = () => (
+ <div id="container">
+    <Title/>
+    {Title()}
+    {title}
+    <h1 className="heading"> Namasthe React Functional Component </h1>
+ </div>
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+root.render(<HeadingComponent/>);
